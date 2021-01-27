c3fc53c30bfc5b21b51c4382355d77861ea68f13
Richard Conway <richard@elastacloud.com>
2021-01-27 20:32:09 +0000

    updated to read from the folder

--- src/CountryTable.py
+++ src/CountryTable.py
@@ -2,6 +2,9 @@ import enum
 from EoraReader import EoraReader
 from PrimaryInputs import PrimaryInputs
 from DomesticTransactions import DomesticTransactions
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
 
 class CountryTableSegment(enum.Enum):
    DomesticTransations = 1
@@ -10,13 +13,31 @@ class CountryTableSegment(enum.Enum):
 class CountryTable(EoraReader):
     def __init__(self, segment, file_path):
         self.segment = segment 
-        self.table_class = None
-        if segment == CountryTableSegment.DomesticTransations:
-            self.table_class = DomesticTransactions(file_path)
-        else: self.table_class = PrimaryInputs(file_path)
+        self.table_class = []
+        self.files = self.__get_files(file_path)
+        self.__process_files(segment, file_path)
+        
+    def __get_files(self, path):
+        if not isfile(path): return [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+        else: return [path]
+
+    def __process_files(self, segment, file_path):
+        for file in self.files:
+            if segment == CountryTableSegment.DomesticTransations:
+                er = DomesticTransactions(file)
+                self.table_class.append(er)
+            else: 
+                er = PrimaryInputs(file)
+                self.table_class.append(er)
 
     def get_dataset(self, extended = False):
-        return self.table_class.get_dataset(extended)
+        df = pd.DataFrame() # empty df here
+        for t_class in self.table_class:
+            if not df.empty:
+                df = df.append(t_class.get_dataset(extended))
+            else: df = t_class.get_dataset(extended)
+        self.df = df
+        return df
 
     def append(self, country_table_part):
         return self.table_class.append(country_table_part.table_class)
--- src/io-table-read.py
+++ src/io-table-read.py
@@ -6,9 +6,5 @@ from CountryTable import CountryTableSegment
 # TODO: replace this with test methods
 if __name__ == "__main__":    
     filepath_1 = sys.argv[1]
-    filepath_2 = sys.argv[2]
     table1 = CountryTable(CountryTableSegment.PrimaryInputs, filepath_1)
-    table2 = CountryTable(CountryTableSegment.PrimaryInputs, filepath_2)
-    df = table2.append(table1)
-    print(df)
-    
+    print(table1.get_dataset(True))
